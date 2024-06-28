373381d74905aa75efaf50449f3b9b834c66c854
Maksim Gromov <maksim.v.gromov@phrm.tech>
2024-06-28 14:18:37 +0700

    Initial Commit

---
+++ DNSLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DNSLibrary
+//
+//  Created by Максим Громов on 28.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
